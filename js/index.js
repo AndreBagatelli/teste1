6de7b53d811dd956e25f131b7bdc02bad0207e44
AndreBagatelli <113361941+AndreBagatelli@users.noreply.github.com>
2022-09-13 16:25:05 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,60 @@
+/* validação de campos */
+  function validateFields(){
+    toggleButtonsDisable();
+    toggleEmailErrors();
+   }
+
+   function login() {
+     firebase.auth().signInWithEmailAndPassword(
+       form.email().value, form.password().value
+     ).then(response => {
+       window.location.href = "/index.html";
+     }).catch(error => {
+       console.log('error', error)
+     });
+
+
+
+   }
+
+  function isEmailValid() {
+    const email = document.getElementById("email").value;
+    if (!email) {
+      return false;
+    }
+    return validateEmail(email);
+
+  }
+
+  function toggleEmailErrors() {
+    const email = document.getElementById('email').value;
+    if (!email) {
+      document.getElementById('email-required-error').style.display = "block";
+    } else {
+      document.getElementById('email-required-error').style.display = "none";
+    }
+
+  }
+
+  function toggleButtonsDisable() {
+
+      const emailValid = isEmailValid();
+      document.getElementById('recover-password-button').disabled = !emailValid;
+
+      const password = isPasswordValid();
+      document.getElementById('login-button').disabled = !emailValid || !passwordValid;
+
+  }
+
+  function isPasswordValid() {
+    const password = document.getElementById('password').value;
+    if (!password) {
+      return false;
+    }
+    return true;
+  }
+
+  function validateEmail(email) {
+    return /\S+@\S+\.\S+/.test(email);
+  }
+    /* validação de campos */
